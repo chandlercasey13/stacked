7b22281af6d730499e6f85d41bcc6681b0a5adf9
Chandler Casey <chandlercasey13@gmail.com>
2026-06-10 11:57:15 -0500

    UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://stacked.example.com",
+  site: "https://www.swecontractinggroup.com",
+  output: "server",
+  adapter: node({ mode: "standalone" }),
+  trailingSlash: "never",
 });
